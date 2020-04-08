655da854064a09d35716b853a12c6a5a3570cbc0
ninjatrex <petersen.mike@protonmail.com>
2020-04-08 16:02:15 +0200

    added comments

--- exercise-1/src/mst/Kruskal.java
+++ exercise-1/src/mst/Kruskal.java
@@ -13,6 +13,8 @@ public class Kruskal {
     private ArrayList<Edge> edgeList;
     private ArrayList<Edge> mstEdgeList;
 
+    // initialization within the constructor is the first phase: sorting edges by
+    // weight
     public Kruskal(EdgeListGraph edgeListGraph) {
         this.edgeList = edgeListGraph.sortEdgeListByWeight(edgeListGraph.getEdgeList());
         this.verticeSet = new UnionFind<Integer>(new TreeSet<Integer>(edgeListGraph.returnEdgeListAsVertexNameList()));
@@ -23,6 +25,7 @@ public class Kruskal {
         return mstEdgeList;
     }
 
+    // second phase: iterate over edges and add edges which haven't been added yet
     public void calcMinSpanningTree() {
         for (Edge edge : this.edgeList) {
             int a = edge.getVertexA().getVertexName();
