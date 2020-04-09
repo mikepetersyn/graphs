a85c146f555217ea83f65f4405d1d8c678142a33
ninjatrex <petersen.mike@protonmail.com>
2020-04-09 23:01:25 +0200

    some adjustments for floydWarshall implementation

--- exercise-1/src/graph/AdjacenceMatrixGraph.java
+++ exercise-1/src/graph/AdjacenceMatrixGraph.java
@@ -15,40 +15,60 @@ public class AdjacenceMatrixGraph extends Graph {
         init(elg.getNumberVertices());
         convertFromEdgeList(elg.getEdgeList());
         super.isDirected = false;
+
     }
 
     public AdjacenceMatrixGraph(EdgeListGraph elg, boolean isDirected) {
         init(elg.getNumberVertices());
         convertFromEdgeList(elg.getEdgeList());
         super.isDirected = isDirected;
+
     }
 
     public AdjacenceMatrixGraph(EdgeListGraph elg, boolean isDirected, boolean isWeighted) {
         init(elg.getNumberVertices());
         convertFromEdgeList(elg.getEdgeList(), isWeighted);
         super.isDirected = isDirected;
+
     }
 
     private void init(int numberVertices) {
         for (int i = 0; i < numberVertices; i++) {
             this.adjacenceMatrix.add(new ArrayList<>());
             for (int j = 0; j < numberVertices; j++) {
-                this.adjacenceMatrix.get(i).add(0);
+                if (i == j)
+                    this.adjacenceMatrix.get(i).add(0);
+                else
+                    // Integer.MAX_VALUE leads to weird operations while doing the condition within
+                    // the floydWarshall
+                    // therefore the value 999999999 represents INFINITY
+                    this.adjacenceMatrix.get(i).add(999999999);
             }
         }
     }
 
-    private void convertFromEdgeList(List<Edge> el) {
+    public void convertFromEdgeList(List<Edge> el) {
         for (Edge e : el) {
             this.adjacenceMatrix.get(e.getVertexA().getVertexName() - 1).set(e.getVertexB().getVertexName() - 1, 1);
         }
     }
 
-    private void convertFromEdgeList(List<Edge> el, boolean isWeighted) {
+    // if there is an edge(i,j) then w(i,j)
+    // if there no edge(i,j) then
+    // if (i,i) then 0 (zero weight on (non-existent) edge between a vertex and
+    // itself)
+    // INFINITY else (infinite weight on non existent edges between two distinct
+    // vertices)
+    public void convertFromEdgeList(List<Edge> el, boolean isWeighted) {
         if (isWeighted) {
+            int i;
+            int j;
+            int w;
             for (Edge e : el) {
-                this.adjacenceMatrix.get(e.getVertexA().getVertexName() - 1).set(e.getVertexB().getVertexName() - 1,
-                        e.getWeight());
+                i = e.getVertexA().getVertexName();
+                j = e.getVertexB().getVertexName();
+                w = e.getWeight();
+                this.adjacenceMatrix.get(i - 1).set(j - 1, w);
             }
         } else
             this.convertFromEdgeList(el);
