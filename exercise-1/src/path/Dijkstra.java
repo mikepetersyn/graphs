00b3c050fe7e7c285232036d3524cb5dce68495b
ninjatrex <petersen.mike@protonmail.com>
2020-04-09 17:02:03 +0200

    implemented dijkstra algo

--- exercise-1/src/path/Dijkstra.java
+++ exercise-1/src/path/Dijkstra.java
@@ -2,8 +2,50 @@ package path;
 
 import java.util.PriorityQueue;
 
+import graph.AdjacenceListGraph;
+import graph.EdgeListGraph;
+import graph.Vertex;
+import mst.VertexKeyComparator;
+
 public class Dijkstra {
 
-    private static final int INFINITY = Integer.MAX_VALUE;
+    private PriorityQueue<Vertex> vPriorityQueue;
+    private VertexKeyComparator vKeyComparator;
+    private EdgeListGraph edgeListGraph;
 
+    public Dijkstra(EdgeListGraph edgeListGraph, AdjacenceListGraph adjacenceListGraph) {
+        this.edgeListGraph = edgeListGraph;
+        this.vKeyComparator = new VertexKeyComparator();
+        this.vPriorityQueue = new PriorityQueue<>(this.edgeListGraph.getNumberVertices(), this.vKeyComparator);
+    }
+
+    public void doIt(int startVertex) {
+        Vertex u;
+        int d;
+        // init
+        this.edgeListGraph.findVertexByName(startVertex).setKey(0);
+        this.vPriorityQueue.addAll(this.edgeListGraph.getVertexList());
+        // start
+        while (!this.vPriorityQueue.isEmpty()) {
+            // extract element with min key (distance)
+            u = this.vPriorityQueue.poll();
+            for (Vertex v : u.getAdjacentVertices()) {
+                // complete distance from Start via Vertex u to Vertex v
+                d = u.getKey() + this.edgeListGraph.findEdgeByVertices(u, v).getWeight();
+                if (d < v.getKey()) {
+                    // update distance and set predecessor of v
+                    v.setKey(d);
+                    v.setPredecessor(u);
+                    // remove and add vertex in order to update the Queue
+                    vPriorityQueue.remove(v);
+                    vPriorityQueue.add(v);
+                }
+            }
+
+        }
+    }
+
+    public void printShortestPath(int startVertex) {
+
+    }
 }
